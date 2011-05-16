dbe0ce527319e5b032dd60cc98d18ec919537591
hly <hly@example.com>
2011-05-16 21:17:40 +0800

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sample3App::Application.config.secret_token = '9ed20afbd19218833c4a3e5e596a31384436756534786c9b9bdeefcb9f8737de630522b80a75d96c3378891c935f673e3db80b7c2a20316a2c74b5632f7f0727'
+Sample3App::Application.config.secret_token = 'ff3f5bcb7f84ce876fbe28687a348ad72da9233ca2aac8d599077119a7f62ec97b98d5d2d674942ce8cb194a7423401ef0a08d1b4830842d350e4d7cf844d04a'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Sample3App::Application.routes.draw do
-  resources :microposts
-
-  resources :users
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
